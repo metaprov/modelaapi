dcf5e39323f55e9c189f83a96d2e3e635c02461a
tmeshorer <tmeshorer@hotmail.com>
2021-02-22 09:00:58 -0800

    fixed js

--- lang/js/lib/index.js
+++
@@ -1,8 +0,0 @@
-import * as _ from 'lodash';
-function component() {
-    var element = document.createElement('div');
-    element.innerHTML = _.join(['Hello', 'webpack'], ' ');
-    return element;
-}
-document.body.appendChild(component());
-//# sourceMappingURL=index.js.map
