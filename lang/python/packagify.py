ebc0ae7d26adebba5fcc353d33cd7505bd84fbc9
tmeshorer <tmeshorer@hotmail.com>
2021-03-02 18:15:30 -0800

    added packages

---
+++ lang/python/packagify.py
@@ -0,0 +1,29 @@
+
+__author__ = """Chris Tabor (dxdstudio@gmail.com)"""
+
+
+from os import walk
+from os import getcwd
+from os import path
+from os import sys
+from glob import glob
+
+sys.path.append(getcwd())
+
+
+def packagify():
+    paths = []
+    start_dir = "github"
+    pattern = "*.py"
+    for dir, subdirs, files in walk(start_dir):
+        # Needs to handle ignored directories
+        print(dir, subdirs, files)
+        if '__init__.py' not in files or len(files) == 0:
+            with open('{}/__init__.py'.format(dir), 'wb+') as initfile:
+                initfile.write(b'\n')
+        paths.extend(glob(path.join(dir, pattern)))
+    return paths
+
+
+
+packagify()
