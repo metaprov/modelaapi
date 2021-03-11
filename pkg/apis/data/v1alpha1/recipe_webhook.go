94d9614124698ce72e7617a207f4da61013ff4cd
tmeshorer <tmeshorer@hotmail.com>
2021-03-11 14:02:42 -0800

    added validator.

--- pkg/apis/data/v1alpha1/recipe_webhook.go
+++ pkg/apis/data/v1alpha1/recipe_webhook.go
@@ -8,6 +8,7 @@ package v1alpha1
 
 import (
 	"github.com/metaprov/modeldapi/pkg/util"
+	"k8s.io/apimachinery/pkg/runtime"
 	"sigs.k8s.io/controller-runtime/pkg/webhook"
 )
 
@@ -24,4 +25,25 @@ func (r *Recipe) Default() {
 		r.Spec.Description = util.StrPtr("")
 	}
 
+}
+
+// validation
+var _ webhook.Validator = &Recipe{}
+
+// ValidateCreate implements webhook.Validator so a webhook will be registered for the type
+func (recipe *Recipe) ValidateCreate() error {
+	return recipe.validate()
+}
+
+// ValidateUpdate implements webhook.Validator so a webhook will be registered for the type
+func (recipe *Recipe) ValidateUpdate(old runtime.Object) error {
+	return recipe.validate()
+}
+
+func (recipe *Recipe) validate() error {
+	return nil
+}
+
+func (recipe *Recipe) ValidateDelete() error {
+	return nil
 }
