f1ff30a515c67d838008b9fb84d0dfa8d3ae527a
tmeshorer <tmeshorer@hotmail.com>
2021-04-18 18:51:57 -0700

    update version

--- lang/python/modeldapi/version.py
+++ lang/python/modeldapi/version.py
@@ -10,7 +10,7 @@ Maintains version and package information for deployment.
 __version_info__ = {
     "major": 0,
     "minor": 3,
-    "micro": 208,   
+    "micro": 209,   
     "releaselevel": "alpha",
     "post": 0,
     "serial": 1,
