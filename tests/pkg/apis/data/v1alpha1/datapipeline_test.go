8ed39e52c030c0b9dbe95c8110f520308f271d78
tmeshorer <tmeshorer@hotmail.com>
2021-05-03 10:19:41 -0700

    fixed

--- tests/pkg/apis/data/v1alpha1/datapipeline_test.go
+++ tests/pkg/apis/data/v1alpha1/datapipeline_test.go
@@ -1,12 +1,8 @@
 package v1alpha1
 
 import (
-	datav1 "github.com/metaprov/modeldapi/pkg/apis/data/v1alpha1"
 	"testing"
 )
 
 func TestDataPipeline(t *testing.T) {
-	pipeline := datav1.DataPipeline{}
-
-	datav1.ParseDataPipelineYaml()
 }
