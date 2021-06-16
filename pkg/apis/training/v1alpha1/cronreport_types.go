3ce78296612f4d2c8d7be3f8f38d8d14b2388351
tmeshorer <tmeshorer@hotmail.com>
2021-06-16 04:11:51 -0700

    added cron report

---
+++ pkg/apis/training/v1alpha1/cronreport_types.go
@@ -0,0 +1,93 @@
+package v1alpha1
+
+import (
+	catalog "github.com/metaprov/modeldapi/pkg/apis/catalog/v1alpha1"
+	v1 "k8s.io/api/core/v1"
+
+	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+)
+
+// PredictionTemplate Condition
+type CronPredictionConditionType string
+
+/// PredictionTemplate Condition
+const (
+	CronPredictionReady CronPredictionConditionType = "Ready"
+	CronPredictionSaved CronPredictionConditionType = "Saved"
+)
+
+// PredictionCondition describes the state of PredictionTemplate
+type CronPredictionCondition struct {
+	// Type of  condition.
+	Type CronPredictionConditionType `json:"type" protobuf:"bytes,1,opt,name=type,casttype=CronPredictionConditionType"`
+	// Status of the condition, one of True, False, Unknown.
+	Status v1.ConditionStatus `json:"status" protobuf:"bytes,2,opt,name=status,casttype=k8s.io/api/core/v1.ConditionStatus"`
+	// Last time the condition transitioned from one status to another.
+	LastTransitionTime *metav1.Time `json:"lastTransitionTime,omitempty" protobuf:"bytes,3,opt,name=lastTransitionTime"`
+	// The reason for the condition's last transition.
+	Reason string `json:"reason,omitempty" protobuf:"bytes,4,opt,name=reason"`
+	// A human readable message indicating details about the transition.
+	Message string `json:"message,omitempty" protobuf:"bytes,5,opt,name=message"`
+}
+
+// +kubebuilder:object:root=true
+// +kubebuilder:subresource:status
+// +kubebuilder:printcolumn:name="Ready",type="string",JSONPath=".status.conditions[?(@.type==\"Ready\")].status"
+// +kubebuilder:printcolumn:name="Owner",type="string",JSONPath=".spec.owner",priority=1
+// +kubebuilder:printcolumn:name="Predictor",type="string",JSONPath=".spec.predictorName"
+// +kubebuilder:printcolumn:name="Cron",type="string",JSONPath=".spec.schedule.cron"
+// +kubebuilder:printcolumn:name="Age",type="date",JSONPath=".metadata.creationTimestamp"
+// +kubebuilder:resource:path=cronpredictions,shortName=cpred,singular=cronprediction,categories={inference,modeld}
+// CronPrediction represents a single run of the Prediction Pipeline
+type CronPrediction struct {
+	metav1.TypeMeta   `json:",inline"`
+	metav1.ObjectMeta `json:"metadata,omitempty" protobuf:"bytes,1,opt,name=metadata"`
+	Spec              CronPredictionSpec   `json:"spec" protobuf:"bytes,2,opt,name=spec"`
+	Status            CronPredictionStatus `json:"status,omitempty" protobuf:"bytes,3,opt,name=status"`
+}
+
+// +kubebuilder:object:root=true
+// CronPredictionList is a list of CronPrediction
+type CronPredictionList struct {
+	metav1.TypeMeta `json:",inline" `
+	metav1.ListMeta `json:"metadata,omitempty" protobuf:"bytes,1,opt,name=metadata"`
+	Items           []CronPrediction `json:"items" protobuf:"bytes,2,rep,name=items"`
+}
+
+type PredictionTemplate struct {
+	Spec PredictionSpec `json:"spec" protobuf:"bytes,1,opt,name=spec"`
+}
+
+// CronPredictionSpec represent the desired state of CronPrediction
+type CronPredictionSpec struct {
+	// VersionName is the data product version of the data pipeline
+	// +kubebuilder:default:=""
+	// +kubebuilder:validation:Optional
+	VersionName *string `json:"versionName,omitempty" protobuf:"bytes,1,opt,name=versionName"`
+	// The owner account name
+	// +kubebuilder:default:="no-one"
+	// +kubebuilder:validation:Optional
+	Owner *string `json:"owner,omitempty" protobuf:"bytes,2,opt,name=owner"`
+	// Schedule is the cron schedule
+	// Schedule for running the pipeline
+	// +kubebuilder:validation:Optional
+	Schedule catalog.RunSchedule `json:"schedule,omitempty" protobuf:"bytes,3,opt,name=schedule"`
+	// Template refer to the prediction template
+	Template PredictionTemplate `json:"template" protobuf:"bytes,4,opt,name=template"`
+}
+
+// CronPredictionStatus is the observed state of a PredictionTemplate
+type CronPredictionStatus struct {
+	// Last run is the last time a run was created
+	//+kubebuilder:validation:Optional
+	LastRun *metav1.Time `json:"lastRun,omitempty" protobuf:"bytes,1,opt,name=lastRun"`
+	// Last time the object was updated
+	//+kubebuilder:validation:Optional
+	LastUpdated *metav1.Time `json:"lastUpdated,omitempty" protobuf:"bytes,2,opt,name=lastUpdated"`
+	// ObservedGeneration is the Last generation that was acted on
+	//+kubebuilder:validation:Optional
+	ObservedGeneration int64 `json:"observedGeneration,omitempty" protobuf:"varint,3,opt,name=observedGeneration"`
+	// Conditions of the cron predictions
+	//+kubebuilder:validation:Optional
+	Conditions []CronPredictionCondition `json:"conditions,omitempty" protobuf:"bytes,4,rep,name=conditions"`
+}
---
+++ pkg/apis/training/v1alpha1/cronreport_webhook.go
@@ -0,0 +1,207 @@
+package v1alpha1
+
+import (
+	"fmt"
+
+	"github.com/metaprov/modeldapi/pkg/apis/data/v1alpha1"
+
+	"github.com/metaprov/modeldapi/pkg/apis/inference"
+	"github.com/metaprov/modeldapi/pkg/util"
+	"gopkg.in/yaml.v2"
+	v1 "k8s.io/api/core/v1"
+	apierrors "k8s.io/apimachinery/pkg/api/errors"
+	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+	"k8s.io/apimachinery/pkg/runtime"
+	"k8s.io/apimachinery/pkg/runtime/schema"
+	"k8s.io/apimachinery/pkg/util/validation/field"
+	ctrl "sigs.k8s.io/controller-runtime"
+	"sigs.k8s.io/controller-runtime/pkg/webhook"
+)
+
+//==============================================================================
+// EntityRef
+//==============================================================================
+
+func (run *CronPrediction) SetupWebhookWithManager(mgr ctrl.Manager) error {
+	return ctrl.NewWebhookManagedBy(mgr).
+		For(run).
+		Complete()
+}
+
+//==============================================================================
+// Keys
+//==============================================================================
+
+func (run *CronPrediction) RootUri() string {
+	return fmt.Sprintf("dataproducts/%s/predictions/%s", run.Namespace, run.Name)
+}
+
+func (run *CronPrediction) ManifestUri() string {
+	return fmt.Sprintf("%s/%s-run.yaml", run.RootUri(), run.Name)
+}
+
+func (run *CronPrediction) InputKey() string {
+	return run.Spec.Template.Spec.Input.Path
+}
+
+func (run *CronPrediction) OutputKey() string {
+	return run.Spec.Template.Spec.Output.Path
+}
+
+//==============================================================================
+// Validate
+//==============================================================================
+
+func (run *CronPrediction) PipelineName() string {
+	return run.ObjectMeta.Labels[PipelineLabelKey]
+}
+
+//==============================================================================
+// Finalizer
+//==============================================================================
+
+func (run *CronPrediction) HasFinalizer() bool {
+	return util.HasFin(&run.ObjectMeta, inference.GroupName)
+}
+func (run *CronPrediction) AddFinalizer() {
+	util.AddFin(&run.ObjectMeta, inference.GroupName)
+}
+func (run *CronPrediction) RemoveFinalizer() {
+	util.RemoveFin(&run.ObjectMeta, inference.GroupName)
+}
+
+//==============================================================================
+// Trackable
+//==============================================================================
+
+// Return the on disk rep location
+func (run *CronPrediction) RepPath(root string) (string, error) {
+	return fmt.Sprintf("%s/predictions/%s.yaml", root, run.ObjectMeta.Name), nil
+}
+
+// Merge or update condition
+// Merge or update condition
+func (run *CronPrediction) CreateOrUpdateCond(cond CronPredictionCondition) {
+	i := run.GetCondIdx(cond.Type)
+	now := metav1.Now()
+	if i == -1 { // not found
+		cond.LastTransitionTime = &now
+		run.Status.Conditions = append(run.Status.Conditions, cond)
+		return
+	}
+	// else we already have the condition, update it
+	current := run.Status.Conditions[i]
+	current.Message = cond.Message
+	current.Reason = cond.Reason
+	current.LastTransitionTime = &now
+	if current.Status != cond.Status {
+		current.Status = cond.Status
+	}
+	run.Status.Conditions[i] = current
+}
+
+func (run *CronPrediction) GetCondIdx(t CronPredictionConditionType) int {
+	for i, v := range run.Status.Conditions {
+		if v.Type == t {
+			return i
+		}
+	}
+	return -1
+}
+
+func (run *CronPrediction) GetCond(t CronPredictionConditionType) CronPredictionCondition {
+	for _, v := range run.Status.Conditions {
+		if v.Type == t {
+			return v
+		}
+	}
+	// if we did not find the condition, we return an unknown object
+	return CronPredictionCondition{
+		Type:    t,
+		Status:  v1.ConditionUnknown,
+		Reason:  "",
+		Message: "",
+	}
+}
+
+func (run *CronPrediction) IsReady() bool {
+	return run.GetCond(CronPredictionReady).Status == v1.ConditionTrue
+}
+
+func (run *CronPrediction) Key() string {
+	return fmt.Sprintf("dataproducts/%s/predictions/%s", run.Namespace, run.Name)
+}
+
+func (run *CronPrediction) ToYamlFile() ([]byte, error) {
+	return yaml.Marshal(run)
+}
+func (run *CronPrediction) OpName() string {
+	return run.Namespace + "-" + run.Name
+}
+
+func (run *CronPrediction) MarkSaved() {
+	run.CreateOrUpdateCond(CronPredictionCondition{
+		Type:   CronPredictionSaved,
+		Status: v1.ConditionTrue,
+	})
+}
+
+func (run *CronPrediction) IsSaved() bool {
+	return run.GetCond(CronPredictionSaved).Status == v1.ConditionTrue
+}
+
+func (run *CronPrediction) MarkReady() {
+	run.CreateOrUpdateCond(CronPredictionCondition{
+		Type:   CronPredictionReady,
+		Status: v1.ConditionTrue,
+	})
+}
+
+func (run *CronPrediction) IsCompleted() bool {
+	return run.GetCond(CronPredictionReady).Status == v1.ConditionTrue
+}
+
+func (run *CronPrediction) IsRunning() bool {
+	cond := run.GetCond(CronPredictionReady)
+	return cond.Status == v1.ConditionFalse && cond.Reason == string(v1alpha1.FeaturePipelineRunPhaseRunning)
+}
+
+func (run *CronPrediction) IsFailed() bool {
+	cond := run.GetCond(CronPredictionReady)
+	return cond.Status == v1.ConditionFalse && cond.Reason == string(v1alpha1.FeaturePipelineRunPhaseFailed)
+}
+
+// defaulting
+var _ webhook.Defaulter = &CronPrediction{}
+
+func (pre *CronPrediction) Default() {
+
+}
+
+// validation
+var _ webhook.Validator = &CronPrediction{}
+
+// ValidateCreate implements webhook.Validator so a webhook will be registered for the type
+func (prediction *CronPrediction) ValidateCreate() error {
+	return prediction.validate()
+}
+
+// ValidateUpdate implements webhook.Validator so a webhook will be registered for the type
+func (prediction *CronPrediction) ValidateUpdate(old runtime.Object) error {
+	return prediction.validate()
+}
+
+func (prediction *CronPrediction) validate() error {
+	var allErrs field.ErrorList
+	if len(allErrs) == 0 {
+		return nil
+	}
+
+	return apierrors.NewInvalid(
+		schema.GroupKind{Group: "inference.modeld.io", Kind: "Prediction"},
+		prediction.Name, allErrs)
+}
+
+func (prediction *CronPrediction) ValidateDelete() error {
+	return nil
+}
