4c6804e1f58cf377aebab7fd900bd5d01428c358
tmeshorer <tmeshorer@hotmail.com>
2021-10-29 08:54:43 -0700

    remove poetry, put the old build back.

--- lang/python/packagify.py
+++ lang/python/packagify.py
@@ -1,4 +1,3 @@
-
 __author__ = """Chris Tabor (dxdstudio@gmail.com)"""
 
 
--- lang/python/setup.py
+++ lang/python/setup.py
@@ -1,53 +1,174 @@
 #!/usr/bin/env python
+# setup
+# Setup script for installing modela
+#
+# Author:   Tomer Sagi
+# Created:  Sun Fev 14 
+#
+# Copyright (C) 2016 The modela developers
+# For license information, see LICENSE.txt and NOTICE.md
+#
 
-"""The setup script."""
 
-from setuptools import setup, find_packages
+"""
+Setup script for installing modela api in python.
+Based on http://bbengfort.github.io/programmer/2016/01/20/packaging-with-pypi.html
+"""
 
-with open('README.md') as readme_file:
-    readme = readme_file.read()
+##########################################################################
+## Imports
+##########################################################################
 
-with open('HISTORY.rst') as history_file:
-    history = history_file.read()
+import os
+import codecs
 
-requirements = ['sphinx', ]
+from setuptools import setup
+from setuptools import find_packages
 
-setup_requirements = [ ]
+##########################################################################
+## Package Information
+##########################################################################
 
-test_requirements = [ ]
+## Basic information
+## Basic information
+NAME         = "modelaapi"
+DESCRIPTION  = "A suite of automatic machine learning for kubernetes"
+AUTHOR       = "The modelaapi developers"
+EMAIL        = "info@modela.ai"
+MAINTAINER   = "The modela developers"
+LICENSE      = "Apache 2"
+REPOSITORY   = "https://github.com/metaptov/modelaapi"
+PACKAGE      = "modelaapi"
+URL          = "http://www.modela.ai"
 
-setup(
-    author="modela",
-    author_email='info@modela.io',
-    python_requires='>=3.8',
-    classifiers=[
-        'Development Status :: 2 - Pre-Alpha',
-        'Intended Audience :: Developers',
-        'License :: OSI Approved :: Apache Software License',
-        'Natural Language :: English',
-        'Programming Language :: Python :: 3',
-        'Programming Language :: Python :: 3.5',
-        'Programming Language :: Python :: 3.6',
-        'Programming Language :: Python :: 3.7',
-        'Programming Language :: Python :: 3.8',
-    ],
-    description="Python server code for modela",
-    entry_points={
-        'console_scripts': [
-            'modela_python_sdk=modela_python_sdk.cli:main',
-        ],
-    },
-    install_requires=requirements,
-    license="Apache Software License 2.0",
-    long_description=readme + '\n\n' + history,
-    include_package_data=True,
-    keywords='modela',
-    name='modela',
-    packages=find_packages(include=['modelaapi', 'modelaapi.*']),
-    setup_requires=setup_requirements,
-    test_suite='tests',
-    tests_require=test_requirements,
-    url='https://github.com/metaprov/mdpyclient',
-    version='0.4.131',
-    zip_safe=False,
+## Define the keywords
+KEYWORDS = (
+    "automl",
+    "machine learning",
+    "data science",
 )
+
+## Define the classifiers
+## See https://pypi.python.org/pypi?%3Aaction=list_classifiers
+CLASSIFIERS  = (
+    "Development Status :: 5 - Production/Stable",
+    "Intended Audience :: Developers",
+    "Intended Audience :: Science/Research",
+    "License :: OSI Approved :: Apache Software License",
+    "Natural Language :: English",
+    "Operating System :: OS Independent",
+    "Programming Language :: Python",
+    "Programming Language :: Python :: 3.5",
+    "Programming Language :: Python :: 3.6",
+    "Programming Language :: Python :: 3.7",
+    "Programming Language :: Python :: 3.8",
+    "Topic :: Software Development",
+    "Topic :: Software Development :: Libraries :: Python Modules",    
+)
+
+## Important Paths
+PROJECT = os.path.abspath(os.path.dirname(__file__))
+REQUIRE_PATH = "requirements.txt"
+VERSION_PATH = os.path.join(PACKAGE, "version.py")
+PKG_DESCRIBE = "DESCRIPTION.md"
+
+## Directories to ignore in find_packages
+EXCLUDES = (
+    "tests", "tests.*",
+    "bin",
+    "docs", "docs.*",
+    "fixtures",
+    "register",
+    "notebooks", "notebooks.*",
+    "examples", "examples.*",
+    "binder", "binder.*",
+    "paper",
+)
+
+##########################################################################
+## Helper Functions
+##########################################################################
+
+
+def read(*parts):
+    """
+    Assume UTF-8 encoding and return the contents of the file located at the
+    absolute path from the REPOSITORY joined with *parts.
+    """
+    with codecs.open(os.path.join(PROJECT, *parts), "rb", "utf-8") as f:
+        return f.read()
+
+
+def get_version(path=VERSION_PATH):
+    """
+    Reads the python file defined in the VERSION_PATH to find the get_version
+    function, and executes it to ensure that it is loaded correctly. Separating
+    the version in this way ensures no additional code is executed.
+    """
+    namespace = {}
+    exec(read(path), namespace)
+    return namespace["get_version"](short=True)
+
+
+def get_requires(path=REQUIRE_PATH):
+    """
+    Yields a generator of requirements as defined by the REQUIRE_PATH which
+    should point to a requirements.txt output by `pip freeze`.
+    """
+    for line in read(path).splitlines():
+        line = line.strip()
+        if line and not line.startswith("#"):
+            yield line
+
+
+def get_description_type(path=PKG_DESCRIBE):
+    """
+    Returns the long_description_content_type based on the extension of the
+    package describe path (e.g. .txt, .rst, or .md).
+    """
+    _, ext = os.path.splitext(path)
+    return {".rst": "text/x-rst", ".txt": "text/plain", ".md": "text/markdown"}[ext]
+
+
+##########################################################################
+## Define the configuration
+##########################################################################
+
+config = {
+    "name": NAME,
+    "version": get_version(),
+    "description": DESCRIPTION,
+    "long_description": read(PKG_DESCRIBE),
+    "long_description_content_type": get_description_type(PKG_DESCRIBE),
+    "classifiers": CLASSIFIERS,
+    "keywords": KEYWORDS,
+    "license": LICENSE,
+    "author": AUTHOR,
+    "author_email": EMAIL,
+    "url": URL,
+    "maintainer": MAINTAINER,
+    "maintainer_email": EMAIL,
+    "project_urls": {
+        "Documentation": URL,
+        "Download": "{}/tarball/v{}".format(REPOSITORY, get_version()),
+        "Source": REPOSITORY,
+        "Tracker": "{}/issues".format(REPOSITORY),
+    },
+    "download_url": "{}/tarball/v{}".format(REPOSITORY, get_version()),
+    "packages": find_packages(where=PROJECT, exclude=EXCLUDES),
+    "package_data": {"yellowbrick": ["datasets/manifest.json"]},
+    "zip_safe": False,
+    "entry_points": {"console_scripts": []},
+    "install_requires": list(get_requires()),
+    "python_requires": ">=3.4, <4",
+    "setup_requires": ["pytest-runner"],
+    "tests_require": ["pytest"],
+}
+
+
+##########################################################################
+## Run setup script
+##########################################################################
+
+if __name__ == "__main__":
+    setup(**config)
