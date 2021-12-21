b5fd305a4f8988124a9cf1d33b7cdace90c06e95
tmeshorer <tmeshorer@hotmail.com>
2021-12-21 13:07:08 -0800

    added the dataapps to the crd list

--- pkg/apis/inference/v1alpha1/dataapp_type.go
+++ pkg/apis/inference/v1alpha1/dataapp_type.go
@@ -41,7 +41,7 @@ type DataAppCondition struct {
 // +kubebuilder:printcolumn:name="Owner",type="string",JSONPath=".spec.owner"
 // +kubebuilder:printcolumn:name="Version",type="string",JSONPath=".spec.versionName"
 // +kubebuilder:printcolumn:name="Age",type="date",JSONPath=".metadata.creationTimestamp",description=""
-// +kubebuilder:resource:path=DataApps,singular=DataApp,categories={data,modela}
+// +kubebuilder:resource:path=dataapps,singular=dataapp,categories={data,modela}
 // +kubebuilder:subresource:status
 type DataApp struct {
 	metav1.TypeMeta   `json:",inline"`
