5530f8a9f28c0f173e688abf948da4aa135fa43d
tmeshorer <tmeshorer@hotmail.com>
2022-01-10 04:24:04 -0800

    use latest version

--- lang/js/src/github.com/metaprov/modelaapi/services/data/v1/data_pb.js
+++ lang/js/src/github.com/metaprov/modelaapi/services/data/v1/data_pb.js
@@ -13,7 +13,13 @@
 
 var jspb = require('google-protobuf');
 var goog = jspb;
-var global = Function('return this')();
+var global = (function() {
+  if (this) { return this; }
+  if (typeof window !== 'undefined') { return window; }
+  if (typeof global !== 'undefined') { return global; }
+  if (typeof self !== 'undefined') { return self; }
+  return Function('return this')();
+}.call(null));
 
 var github_com_metaprov_modelaapi_pkg_apis_data_v1alpha1_generated_pb = require('../../../../../../github.com/metaprov/modelaapi/pkg/apis/data/v1alpha1/generated_pb.js');
 goog.object.extend(proto, github_com_metaprov_modelaapi_pkg_apis_data_v1alpha1_generated_pb);
--- lang/js/src/k8s.io/apimachinery/pkg/runtime/generated_pb.js
+++ lang/js/src/k8s.io/apimachinery/pkg/runtime/generated_pb.js
@@ -13,7 +13,13 @@
 
 var jspb = require('google-protobuf');
 var goog = jspb;
-var global = Function('return this')();
+var global = (function() {
+  if (this) { return this; }
+  if (typeof window !== 'undefined') { return window; }
+  if (typeof global !== 'undefined') { return global; }
+  if (typeof self !== 'undefined') { return self; }
+  return Function('return this')();
+}.call(null));
 
 goog.exportSymbol('proto.k8s.io.apimachinery.pkg.runtime.RawExtension', null, global);
 goog.exportSymbol('proto.k8s.io.apimachinery.pkg.runtime.TypeMeta', null, global);
