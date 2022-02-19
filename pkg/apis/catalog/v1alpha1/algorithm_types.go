9838d6bbc07ff2fe8a448097047ba1f7c91e929f
tmeshorer <tmeshorer@hotmail.com>
2022-02-19 10:46:39 -0800

    changed cast type

--- pkg/apis/catalog/v1alpha1/algorithm_types.go
+++ pkg/apis/catalog/v1alpha1/algorithm_types.go
@@ -48,13 +48,13 @@ type AlgorithmSpec struct {
 	Sparse *bool `json:"sparse,omitempty" protobuf:"bytes,5,opt,name=sparse"`
 	// IntegerParameters is the list of integer hyper parameter ranges.
 	// +kubebuilder:validation:Optional
-	IntegerParameters []IntParameter `json:"integerParameters,omitempty" protobuf:"bytes,6,rep,name=integerParameters,casttype=integerParameters"`
+	IntegerParameters []IntParameter `json:"integerParameters,omitempty" protobuf:"bytes,6,rep,name=integerParameters,casttype=IntegerParameters"`
 	// FloatParameters is the list of float hyper parameters ranges
 	// +kubebuilder:validation:Optional
-	FloatParameters []FloatParameter `json:"floatParameters,omitempty" protobuf:"bytes,7,rep,name=floatParameters,casttype=floatParameters"`
+	FloatParameters []FloatParameter `json:"floatParameters,omitempty" protobuf:"bytes,7,rep,name=floatParameters,casttype=FloatParameters"`
 	// CategoricalParameters is the list of categorical hyper parameters ranges
 	// +kubebuilder:validation:Optional
-	CategoricalParameters []CategoricalParameter `json:"categoricalParameters,omitempty" protobuf:"bytes,8,rep,name=categoricalParameters,casttype=categoricalParameters"`
+	CategoricalParameters []CategoricalParameter `json:"categoricalParameters,omitempty" protobuf:"bytes,8,rep,name=categoricalParameters,casttype=CategoricalParameters"`
 }
 
 // IntParameter contain the specification of an integer hyper parameter
