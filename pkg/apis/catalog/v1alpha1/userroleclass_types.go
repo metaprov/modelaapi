a2a5a02aef11e75bf457f6b68e767db870ac244d
tmeshorer <tmeshorer@hotmail.com>
2022-03-02 10:31:51 -0800

    added resource names

--- pkg/apis/catalog/v1alpha1/userroleclass_types.go
+++ pkg/apis/catalog/v1alpha1/userroleclass_types.go
@@ -13,7 +13,70 @@ import (
 type ResourceName string
 
 const (
-	AccountResourceName string = "account"
+	// infra resources
+	AccountResourceName    string = "account"
+	AlertResourceName      string = "alert"
+	AttachmentResourceName string = "attachment"
+	CommitResourceName     string = "commit"
+	ConnectionResourceName string = "connection"
+	LabResourceName        string = "lab"
+	LicenseResourceName    string = "license"
+	ModelaSystemeName      string = "modelasystem"
+	NotifierName           string = "notifier"
+	ServingSiteName        string = "servingsite"
+	TenantName             string = "tenant"
+	VirtualBucketName      string = "virtualbucket"
+	VirtualClusterName     string = "virtualcluster"
+	VirtualVolumeName      string = "virtualvolume"
+
+	// data resources
+	DataPipelineResourceName        string = "datapipeline"
+	DataPipelineRunResourceName     string = "datapipelinerun"
+	DataProductResourceName         string = "dataproduct"
+	DataProductVersionResourceName  string = "dataproductversion"
+	DatasetResourceName             string = "dataset"
+	DatasourceResourceName          string = "datasource"
+	EntityResourceName              string = "entity"
+	FeatureHistorgramResourceName   string = "featurehistorgram"
+	FeatureResourceName             string = "feature"
+	FeaturePipelineResourceName     string = "featurepipeline"
+	FeaturePipelineRunResourceName  string = "featurepipelinerun"
+	FeaturesetResourceName          string = "featureset"
+	LabelingPipelineResourceName    string = "labelingpipeline"
+	LabelingPipelineRunResourceName string = "labelingpipelinerun"
+	RecipeResourceName              string = "recipe"
+	RecipeRunResourceName           string = "reciperun"
+	ReportResourceName              string = "report"
+	SqlQueryResourceName            string = "sqlquery"
+	SqlQueryRunResourceName         string = "sqlqueryrun"
+	WebRequestResourceName          string = "webrequest"
+	WebRequestRunResourceName       string = "webrequestrun"
+
+	// Team
+	MeetingResourceName    string = "meeting"
+	PostmortemResourceName string = "postmortem"
+	ReviewResourceName     string = "review"
+	RunbookResourceName    string = "runbook"
+	TodoResourceName       string = "todo"
+
+	// Training
+	CronReportResourceName       string = "cronreport"
+	ModelAutoBuilderResourceName string = "modelautobuilder"
+	ModelPipelineResourceName    string = "modelpipeline"
+	ModelPipelineRunResourceName string = "modelpipelinerun"
+	ModelResourceName            string = "model"
+	ModelCompilerRunName         string = "modelcompilerrun"
+	NotebookName                 string = "notebook"
+	NotebookRunName              string = "notebookrun"
+	ReportName                   string = "report"
+	StudyName                    string = "study"
+
+	// Inference
+	CronPredictionResourceName string = "cronprediction"
+	CurtainResourceName        string = "curtain"
+	DataAppResourceName        string = "dataapp"
+	PredictionResourceName     string = "prediction"
+	PredictorResourceName      string = "predictor"
 )
 
 type ActionName string
@@ -24,6 +87,7 @@ const (
 	GetActionName    ActionName = "get"
 	UpdateActionName ActionName = "update"
 	ListActionName   ActionName = "list"
+	RunActionName    ActionName = "run"
 	AllActionName    ActionName = "*"
 )
 
