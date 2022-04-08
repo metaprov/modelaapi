3c8082aed3b8a2faca998a5216fc789b11903413
tmeshorer <tmeshorer@hotmail.com>
2022-04-08 07:56:24 -0700

    issue 43

--- pkg/apis/inference/v1alpha1/prediction_webhook.go
+++ pkg/apis/inference/v1alpha1/prediction_webhook.go
@@ -1,6 +1,8 @@
 package v1alpha1
 
 import (
+	data "github.com/metaprov/modelaapi/pkg/apis/data/v1alpha1"
+	"github.com/metaprov/modelaapi/pkg/util"
 	apierrors "k8s.io/apimachinery/pkg/api/errors"
 	"k8s.io/apimachinery/pkg/runtime"
 	"k8s.io/apimachinery/pkg/runtime/schema"
@@ -12,6 +14,13 @@ import (
 var _ webhook.Defaulter = &Prediction{}
 
 func (prediction *Prediction) Default() {
+	// auto generate prediction output
+	if *prediction.Spec.Output.Location.Type == data.DataLocationObjectStorage {
+		if *prediction.Spec.Output.Location.Path == "" {
+			prediction.Spec.Output.Location.Path = util.StrPtr(*prediction.Spec.Input.Location.Path + ".out")
+		}
+	}
+
 	// if no input bucket defined,
 	if prediction.ObjectMeta.Labels == nil {
 		prediction.ObjectMeta.Labels = make(map[string]string)
