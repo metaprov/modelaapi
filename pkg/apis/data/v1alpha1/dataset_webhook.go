88c37fe6f16e60dee838ae41c33062ab9db18518
tmeshorer <tmeshorer@hotmail.com>
2022-06-08 10:45:40 -0700

    added support for web url

--- pkg/apis/data/v1alpha1/dataset_webhook.go
+++ pkg/apis/data/v1alpha1/dataset_webhook.go
@@ -19,8 +19,6 @@ var _ webhook.Defaulter = &Dataset{}
 
 func (dataset *Dataset) Default() {
 
-	// if not row, we assume is 0
-
 	// if we read from a database, we must snapshot the data. by default we do not snapshot
 	if *dataset.Spec.Origin.Type == DataLocationSQLTable ||
 		*dataset.Spec.Origin.Type == DataLocationSQLView ||
