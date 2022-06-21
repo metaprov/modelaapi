6c5b14f80a7141065b551497ba6feb98f6d89b0d
tmeshorer <tmeshorer@hotmail.com>
2022-06-21 11:33:03 -0700

    set kind to clusterrole

--- pkg/apis/infra/v1alpha1/servingsite_lifecycle.go
+++ pkg/apis/infra/v1alpha1/servingsite_lifecycle.go
@@ -385,7 +385,7 @@ func (servingsite *ServingSite) PredictorRoleBinding() *rbacv1.ClusterRoleBindin
 		},
 		RoleRef: rbacv1.RoleRef{
 			APIGroup: "rbac.authorization.k8s.io",
-			Kind:     "Role",
+			Kind:     "ClusterRole",
 			Name:     catalog.ServingSitePredictorRole,
 		},
 	}
