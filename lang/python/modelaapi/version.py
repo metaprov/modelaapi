365467ae7bca5eda149b6f3b279961c552859e39
tmeshorer <tmeshorer@hotmail.com>
2022-07-19 15:18:53 -0700

    upgrade

--- lang/python/modelaapi/version.py
+++ lang/python/modelaapi/version.py
@@ -9,8 +9,8 @@ Maintains version and package information for deployment.
 
 __version_info__ = {
     "major": 0,
-    "minor": 4,
-    "micro": 727,   
+    "minor": 5,
+    "micro": 0,   
     "releaselevel": "alpha",
     "post": 0,
     "serial": 1,
