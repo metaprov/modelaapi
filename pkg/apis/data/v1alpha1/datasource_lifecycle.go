76f0991508b4fc8aa08499770bec0a9fc0b9a8cc
tmeshorer <tmeshorer@hotmail.com>
2022-07-22 14:44:54 -0700

    fixed the validation rule method

--- pkg/apis/data/v1alpha1/datasource_lifecycle.go
+++ pkg/apis/data/v1alpha1/datasource_lifecycle.go
@@ -274,6 +274,6 @@ func (sc *DataSource) Saved() bool {
 }
 
 func (sc *DataSource) HaveValidationRules() bool {
-	return len(sc.Spec.Schema.Tests.Tests) > 0
+	return len(sc.Spec.TestsTemplate.Tests) > 0
 
 }
