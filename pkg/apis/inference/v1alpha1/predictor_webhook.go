9ab1a8eca27dbdb5379ad6cfa06456bc60326e50
tmeshorer <tmeshorer@hotmail.com>
2022-08-01 08:31:27 -0700

    removed drift thresholds

--- pkg/apis/inference/v1alpha1/predictor_webhook.go
+++ pkg/apis/inference/v1alpha1/predictor_webhook.go
@@ -2,7 +2,6 @@ package v1alpha1
 
 import (
 	catalog "github.com/metaprov/modelaapi/pkg/apis/catalog/v1alpha1"
-	data "github.com/metaprov/modelaapi/pkg/apis/data/v1alpha1"
 	"github.com/metaprov/modelaapi/pkg/util"
 	v1 "k8s.io/api/core/v1"
 	"k8s.io/apimachinery/pkg/runtime"
@@ -41,9 +40,6 @@ func (predictor *Predictor) Default() {
 
 	if predictor.ObjectMeta.Labels == nil {
 		predictor.ObjectMeta.Labels = make(map[string]string)
-	}
-	if predictor.Spec.Drift.Thresholds == nil {
-		predictor.Spec.Drift.Thresholds = make([]data.DriftThreshold, 0)
 	}
 	predictor.ObjectMeta.Labels[catalog.TenantLabelKey] = predictor.Spec.ServingSiteRef.Namespace
 	predictor.ObjectMeta.Labels["modela.ai/servingsite"] = predictor.Spec.ServingSiteRef.Name
