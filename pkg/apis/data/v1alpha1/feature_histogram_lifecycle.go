7c97b76e8430b5a44a7e0d1fe3a428c6b923aa6f
tmeshorer <tmeshorer@hotmail.com>
2022-08-07 06:44:22 -0700

    fixed expired rule

--- pkg/apis/data/v1alpha1/feature_histogram_lifecycle.go
+++ pkg/apis/data/v1alpha1/feature_histogram_lifecycle.go
@@ -356,7 +356,7 @@ func (fh *FeatureHistogram) DriftAlert(tenantRef *v1.ObjectReference, notifierNa
 // check if we need to expire the feature histogram
 func (fh *FeatureHistogram) ShouldExpire(maxPredictions int32) bool {
 	// calc total points
-	return *fh.Spec.Live && fh.Status.Total > maxPredictions && fh.Expired()
+	return *fh.Spec.Live && fh.Status.Total >= maxPredictions && fh.Expired()
 }
 
 // check if we should compute drift, we should compute drift,
