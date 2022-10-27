daf17ad340acbb0e9245a4bcd70554b8391b715a
tmeshorer <tmeshorer@hotmail.com>
2022-10-27 14:30:35 -0700

    rename the tasks

--- pkg/apis/data/v1alpha1/suite_test.go
+++ pkg/apis/data/v1alpha1/suite_test.go
@@ -7,11 +7,6 @@ import (
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 )
 
-func TestInfraObjects(t *testing.T) {
-	RegisterFailHandler(Fail)
-	RunSpecs(t, "infra objects specs")
-}
-
 func Test_Date(t *testing.T) {
 	now := metav1.Now()
 	result := now.Format("01/2/2006 15:04:05")
